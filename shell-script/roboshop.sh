9f9db5c81ff76f06a334c750aa7fcbca53ea0b0b
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2020-09-28 02:14:45 +0500

    initial project state

---
+++ shell-script/roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+case $1 in
+  frontend)
+    echo Installing Frontend
+    echo Completed Installing Frontend
+    ;;
+  catalogue)
+   echo  Installing Catalogue
+   echo Completed Installing Catalogue
+   ;;
+ cart)
+   echo  Installing Cart
+   echo  Completed Cart
+   ;;
+  *)
+;;
+esac

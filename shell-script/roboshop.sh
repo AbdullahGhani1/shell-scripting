0c3ab24690880a0805203731dbf5dc83ead06c3e
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2020-09-29 22:49:42 +0500

    frontend-script

--- shell-script/roboshop.sh
+++ shell-script/roboshop.sh
@@ -13,6 +13,10 @@ case $USER_ID in
   ;;
 esac
 ## Function
+Print(){
+    echo -e "\e[1;33m**********>>>>>>>>>>>> $1 <<<<<<<<<<***********\e[0m"
+
+}
 statusCheck(){
   case $? in
     0)
@@ -24,15 +28,27 @@ statusCheck(){
       ;;
   esac
 }
+# Main Program
 case $1 in
   frontend)
-    echo -e "\e[1;33m**********>>>>>>>>>>>> Installing Nginx <<<<<<<<<<***********\e[0m"
+    Print "Installing Nginx"
     yum install nginx -y
     statusCheck
-    echo -e "\e[1;33m**********>>>>>>>>>>>> Starting Nginx <<<<<<<<<<***********\e[0m"
+    statusCheck
+    Print "Downloading Frontend App"
+    curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/db389ddc-b576-4fd9-be14-b373d943d6ee/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+    statusCheck
+     cd /usr/share/nginx/html
+     rm -rf *
+     Print "Extracting Frontend Archive"
+     unzip /tmp/frontend.zip
+     mv static/* .
+    rm -rf static README.md
+    mv localhost.conf /etc/nginx/nginx.conf
+    Print "Starting Nginx"
     systemctl enable nginx
     systemctl start nginx
-    statusCheck
+
     ;;
   catalogue)
    echo  Installing Catalogue
